0be1d88a664959a76f4f89ab1aa5404df3f385ef
sivatfl23 <sivatfl23@gmail.com>
2017-03-21 15:55:27 +0000

    added refund file

--- src/test/java/uk/gov/hmcts/payment/acceptancetests/RefundPaymentIntegrationTest.java
+++ src/test/java/uk/gov/hmcts/payment/acceptancetests/RefundPaymentIntegrationTest.java
@@ -1,22 +1,20 @@
 package uk.gov.hmcts.payment.acceptancetests;
 
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.gov.hmcts.payment.acceptancetests.dsl.PaymentTestDsl;
 import uk.gov.hmcts.payment.api.contract.CreatePaymentRequestDto;
 import uk.gov.hmcts.payment.api.contract.PaymentDto;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import uk.gov.hmcts.payment.api.contract.RefundPaymentRequestDto;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 import static uk.gov.hmcts.payment.api.contract.CreatePaymentRequestDto.createPaymentRequestDtoWith;
+import static uk.gov.hmcts.payment.api.contract.RefundPaymentRequestDto.refundPaymentRequestDtoWith;
 
 /**
- * Created by kknuthalapati on 15/03/2017.
+ * Created by Sivakumar Sirigiri on 20/03/2017.
  */
-public class GetPaymentIntegrationTest extends IntegrationTestBase {
+public class RefundPaymentIntegrationTest extends IntegrationTestBase {
 
     private CreatePaymentRequestDto.CreatePaymentRequestDtoBuilder validRequest = createPaymentRequestDtoWith()
             .amount(100)
@@ -25,59 +23,49 @@ public class GetPaymentIntegrationTest extends IntegrationTestBase {
             .reference("Reference")
             .returnUrl("https://return-url");
 
+    private RefundPaymentRequestDto.RefundPaymentRequestDtoBuilder refundValidRequest = refundPaymentRequestDtoWith()
+            .amount(5)
+            .refundAmountAvailable(100);
+
+    private RefundPaymentRequestDto.RefundPaymentRequestDtoBuilder refundAmountAvailalbeInvalidRequest = refundPaymentRequestDtoWith()
+            .amount(5)
+            .refundAmountAvailable(1000);
+
+    private RefundPaymentRequestDto.RefundPaymentRequestDtoBuilder refundAmountInvalidRequest = refundPaymentRequestDtoWith()
+            .amount(5000)
+            .refundAmountAvailable(100);
+
+
     @Autowired
     private PaymentTestDsl scenario;
 
     @Test
-    public void validGetPaymentRequestShouldResultIn200() throws IOException {
+    public void createAndRefundPayment() throws IOException {
         AtomicReference<PaymentDto> paymentHolder = new AtomicReference<>();
-
         scenario.given().userId("1").serviceId("divorce")
                 .when()
                 .createPayment("1", validRequest, paymentHolder)
-                .getPayment("1", paymentHolder.get().getId())
-                .then().get((paymentDto -> {
-                    assertThat(paymentDto.getAmount()).isEqualTo(100);
-                    assertThat(paymentDto.getState()).isEqualTo(new PaymentDto.StateDto("created", false));
-                    assertThat(paymentDto.getDescription()).isEqualTo("Description");
-                    assertThat(paymentDto.getReference()).isEqualTo("Reference");
-                    assertThat(paymentDto.getLinks().getCancel().getHref()).endsWith(paymentHolder.get().getId() + "/cancel");
-                    assertThat(paymentDto.getLinks().getNextUrl()).isNotNull();
-                })
-        );
+                .refundPayment("1",refundValidRequest,paymentHolder.get().getId())
+                .then().refundPayment();
     }
 
     @Test
-    public void createAndCancelApproachC() throws IOException {
+    public void createAndRefundAvaialbleAmountInvalid() throws IOException {
         AtomicReference<PaymentDto> paymentHolder = new AtomicReference<>();
-
         scenario.given().userId("1").serviceId("divorce")
                 .when()
                 .createPayment("1", validRequest, paymentHolder)
-                .cancelPayment("1", paymentHolder.get().getId())
-                .then().cancelled();
+                .refundPayment("1",refundAmountAvailalbeInvalidRequest,paymentHolder.get().getId())
+                .then().refundAvailableAmountInvalid412();
     }
 
-
     @Test
-    public void getPaymentWithOutServiceIdRequestShouldResultIn500() throws IOException {
+    public void createAndRefundAmountInvalid() throws IOException {
+        AtomicReference<PaymentDto> paymentHolder = new AtomicReference<>();
         scenario.given().userId("1").serviceId("divorce")
-                .when().getPayment("1", "")
+                .when()
+                .createPayment("1", validRequest, paymentHolder)
+                .refundPayment("1",refundAmountInvalidRequest,paymentHolder.get().getId())
                 .then().validationErrorfor500("");
     }
-
-    @Test
-    public void getPaymentWithOutUserIdRequestShouldResultIn404() throws IOException {
-        scenario.given().userId("1").serviceId("divorce")
-                .when().getPayment("", "9")
-                .then().notFound();
-    }
-
-    @Test
-    public void getPaymentWithOutUserIdAndPaymentIdRequestShouldResultIn400() throws IOException {
-        scenario.given().userId("1").serviceId("divorce")
-                .when().getPayment("", "")
-                .then().notFound();
-    }
-
 }
